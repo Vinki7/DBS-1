f7231ee604c18a06139b2870ba1d8621b583c27f
Vinki7 <xvinklers@stuba.sk>
2025-03-15 10:38:16 +0100

    4th query should be done
    necessary fix: change count at line 24 for sum & change value from 1 to 2 at line 27 to calculate correct values

--- vypracovanie/4.sql
+++ vypracovanie/4.sql
@@ -1,76 +1,153 @@
-/*
-Triple Double = 3 typy statistik, ktore dosiahli 2-cifernu hodnotu v jendom zapase
-Focus teraz len na pocet bodov, asistencie, doskoky
-    points, assists, rebounds
-
-1. Vyebraju sa vsetci hraci pre vybranu sezonu, kotry dosiahli triple double
-2. Pre vybranych hracov treba urcit najdlhsiu seriu triple double bez prerusenia
-    - ak dva zapasy posebe triple double, seria = 2
-    - ak v dalsom zapase da opat triple double, seria sa natahuje na 3, inak ostava 2
-    - ak je seria prerusena a hrac da triple double, zacina sa zasa od 1
-
-Vypocet:
-    'FIELD_GOAL_MADE' = 2 body
-    'FREE_THROW' = 1 ak bol premeneny
-    'REBOUND' = pocita sa kazdy hadam
-
-Vystup:
-player_id | longest_streak
-order:
-1. longest_streak DESC
-2. player_id ASC
-*/
-
+-- Active: 1740996226560@@localhost@5433@nba@public
 WITH selected_records as (
     SELECT
-        pr.player1_id AS player1_id,
-        pr.player2_id AS player2_id,
-        pr.event_number AS event_number,
+        pl.id AS player_id,
+        pr.player1_id,
+        pr.player2_id,
+        pr.event_number,
         pr.event_msg_type AS event_type,
-        pr.score AS score,
-        pr.game_id AS game_id
-    FROM play_records AS pr
+        pr.score,
+        pr.game_id
+    FROM players AS pl
+    JOIN play_records AS pr ON pl.id = pr.player1_id OR pl.id = pr.player2_id
     JOIN games ON games.id = pr.game_id
     WHERE 
         games.season_id = '22018'--{{season_id}}
         AND
         pr.event_msg_type IN ('FIELD_GOAL_MADE', 'FREE_THROW', 'REBOUND')
-    ORDER BY pr.game_id ASC, pr.player1_id ASC, pr.player2_id ASC
+    ORDER BY pr.game_id ASC, pr.player1_id ASC
+),
+game_statistics AS (
+    SELECT
+        sr.player_id AS player_id,
+        sr.game_id AS game_id,
+        (SUM(
+            CASE 
+                WHEN (sr.event_type = 'FIELD_GOAL_MADE' AND sr.player_id = sr.player1_id) 
+                    THEN 2
+                ELSE 0
+            END
+        )
+        +
+        COUNT(
+            CASE 
+                WHEN (sr.event_type = 'FREE_THROW' AND sr.player_id = sr.player1_id AND sr.score IS NOT NULL) 
+                    THEN 1
+            END
+        )) AS points,
+        COUNT(
+            CASE
+                WHEN (sr.event_type = 'FIELD_GOAL_MADE' AND sr.player_id = sr.player2_id)
+                    THEN 1
+            END
+        ) AS assists_made,
+        COUNT(
+            CASE
+                WHEN (sr.event_type = 'REBOUND')
+                    THEN 1
+            END
+        ) AS rebounds_made
+    FROM selected_records AS sr
+    GROUP BY sr.game_id, sr.player_id
+),
+is_td AS (
+    SELECT
+        game_id,
+        player_id,
+        CASE
+            WHEN points >= 10 AND assists_made >= 10 AND rebounds_made >= 10
+                THEN 1
+            ELSE 0
+        END AS is_triple_double
+    FROM game_statistics
+),
+streak_groups AS (
+    SELECT
+        st.player_id,
+        st.game_id,
+        st.is_triple_double,
+        st.streak_start,
+        SUM(st.streak_start) OVER (PARTITION BY st.player_id ORDER BY st.game_id) AS streak_group
+    FROM (
+        SELECT 
+            pl.player_id,
+            td.game_id,
+            td.is_triple_double,
+            CASE 
+                WHEN td.is_triple_double = 1
+                AND LAG(td.is_triple_double, 1, 0) OVER (PARTITION BY pl.player_id ORDER BY td.game_id) = 0 
+                    THEN 1
+                ELSE 0
+            END AS streak_start
+
+        FROM (
+            SELECT DISTINCT player_id
+            FROM is_td
+            GROUP BY game_id, player_id
+            HAVING SUM(is_triple_double) > 0
+            ORDER BY player_id ASC
+        ) AS pl
+        JOIN is_td AS td ON td.player_id = pl.player_id
+    ) AS st
 )
 SELECT
-    pl.id AS player_id,
-    sr.game_id AS game_id,
-    COUNT(
-        CASE 
-            WHEN (sr.event_type = 'FIELD_GOAL_MADE' AND pl.id = sr.player1_id) 
-                THEN 1
-        END
-    ) AS field_goals_made,
-    COUNT(
-        CASE
-            WHEN (sr.event_type = 'FIELD_GOAL_MADE' AND pl.id = sr.player2_id)
-                THEN 1
-        END
-    ) AS assists_made,
-    COUNT(
-        CASE 
-            WHEN (sr.event_type = 'FREE_THROW' AND sr.score IS NOT NULL) 
-                THEN 1
-        END
-    ) AS free_throws_scored,
-    COUNT(
-        CASE
-            WHEN (sr.event_type = 'REBOUND')
-                THEN 1
-        END
-    ) AS rebounds_made
-FROM selected_records AS sr
-JOIN players AS pl ON pl.id = (
-        CASE 
-            WHEN pl.id = sr.player1_id 
-                THEN sr.player1_id 
-            WHEN pl.id = sr.player2_id 
-                THEN sr.player2_id  
-        END
-    )
-GROUP BY sr.game_id, pl.id;
+    player_id,
+    MAX(longest_streak) AS longest_streak
+FROM (
+    SELECT
+        sg.player_id,
+        sg.streak_group,
+        COUNT(*) AS longest_streak
+    FROM streak_groups AS sg
+    WHERE sg.is_triple_double = 1
+    GROUP BY sg.player_id, sg.streak_group
+    ORDER BY sg.player_id DESC, sg.streak_group ASC
+)
+GROUP BY player_id
+ORDER BY longest_streak DESC, player_id ASC;
+,
+-- streaks AS (
+--     SELECT
+--         pl.player_id,
+--         td.game_id,
+--         td.is_triple_double,
+--         CASE 
+--             WHEN td.is_triple_double = 1 
+--             AND LAG(td.is_triple_double, 1, 0) OVER (PARTITION BY pl.player_id ORDER BY td.game_id) = 0
+--                 THEN 1
+--             ELSE 0  
+--             END AS streak_start
+--     FROM (
+--         SELECT DISTINCT player_id
+--         FROM is_td
+--         GROUP BY game_id, player_id
+--         HAVING SUM(is_triple_double) > 0
+--         ORDER BY player_id ASC
+--     ) AS pl
+--     JOIN is_td AS td ON td.player_id = pl.player_id
+-- ),
+-- grouped_streaks AS (
+--     SELECT
+--         player_id,
+--         game_id,
+--         is_triple_double,
+--         streak_start,
+--         SUM(streak_start) OVER (PARTITION BY player_id ORDER BY game_id) AS streak_group
+--     FROM streaks
+-- )
+-- SELECT
+--     player_id,
+--     MAX(longest_streak) AS longest_streak
+-- FROM (
+--     SELECT
+--         player_id,
+--         streak_group,
+--         COUNT(*) AS longest_streak
+--     FROM grouped_streaks
+--     WHERE is_triple_double = 1
+--     GROUP BY player_id, streak_group
+--     ORDER BY player_id DESC, streak_group ASC
+-- )
+-- GROUP BY player_id
+-- ORDER BY longest_streak DESC, player_id ASC
+
