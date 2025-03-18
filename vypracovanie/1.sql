eb6b9ec22df6dc2fcb715ee2d7acf558d6d6add7
Vinki7 <xvinklers@stuba.sk>
2025-03-18 18:50:53 +0100

    The first query remade and fixed

--- vypracovanie/1.sql
+++ vypracovanie/1.sql
@@ -1,21 +1,25 @@
-WITH p_selection AS (
-    SELECT 
-        pr1.player1_id AS player_id,
-        pr2.period AS period,
-        pr2.pctimestring AS period_time
-    FROM play_records AS pr1
-        JOIN play_records AS pr2 
-            ON pr1.game_id = pr2.game_id
-            AND pr1.player1_id = pr2.player1_id
-            AND pr2.event_number = pr1.event_number + 1
-    WHERE pr1.game_id = {{game_id}} AND pr1.event_msg_type = 'REBOUND' AND pr2.event_msg_type = 'FIELD_GOAL_MADE'--22000516, 22000520, 22000521, 22000529
+WITH selection AS (
+    SELECT
+        pr.player1_id AS player_id,
+        pr.period AS period,
+        pr.pctimestring AS period_time,
+        pr.event_msg_type AS event_type,
+        pr.event_number AS event_number,
+        LAG(pr.event_msg_type, 1) 
+        OVER (ORDER BY pr.event_number) AS prev_event,
+        LAG(pr.player1_id, 1)
+        OVER (ORDER BY pr.event_number) AS prev_player
+    FROM play_records AS pr
+    WHERE pr.game_id = {{game_id}}::INTEGER--22000516, 22000529
+    ORDER BY pr.event_number
 )
 SELECT 
-    p_selection.player_id, 
-    players.first_name,
-    players.last_name,
-    p_selection.period, 
-    p_selection.period_time
-FROM p_selection
-    JOIN players ON p_selection.player_id = players.id
-ORDER BY p_selection.period ASC, p_selection.period_time DESC;
+    sel.player_id, 
+    pl.first_name,
+    pl.last_name,
+    sel.period, 
+    sel.period_time
+FROM selection AS sel
+JOIN players AS pl ON sel.player_id = pl.id
+WHERE sel.player_id = sel.prev_player AND (event_type = 'FIELD_GOAL_MADE' AND prev_event = 'REBOUND')
+ORDER BY sel.period ASC, sel.period_time DESC;
